56fd129c011896e247c395fb6034c3555687e129
XP <smurfs@smurfs.verymad.net>
2025-04-23 17:10:27 -0700

    Update workflow to build non-master branches

--- src/main/java/gg/xp/Backup.java
+++ src/main/java/gg/xp/Backup.java
@@ -14,6 +14,7 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.UUID;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Backup {
 	private static final Logger log = LoggerFactory.getLogger(Backup.class);
@@ -36,14 +37,17 @@ public class Backup {
 				throw new IllegalStateException("Could not create directory " + outdir.getAbsolutePath());
 			}
 		}
-		log.info("Row count: {}", db.countRows());
+		long rowCount = db.countRows();
+		log.info("Row count: {}", rowCount);
 
 		Path outPath = outdir.toPath();
+		AtomicInteger counter = new AtomicInteger();
 		db.iterateAll((v) -> {
 			UUID uuid = UUID.fromString(v.get("linkuuid").getString());
 			Path out = outPath.resolve(uuid + ".json");
 			try {
 				Files.writeString(out, v.toJson(), StandardCharsets.UTF_8);
+				counter.incrementAndGet();
 				// TODO: possibly redundant now that it has proper unit throttling
 				Thread.sleep(10);
 			}
@@ -51,6 +55,7 @@ public class Backup {
 				throw new RuntimeException(e);
 			}
 		});
+		log.info("Backup completed. Total rows: {}, Expected rows: {}", counter.get(), rowCount);
 	}
 
 	public static void main(String[] args) {
@@ -68,6 +73,8 @@ public class Backup {
 		try {
 			Backup backup = pico.getComponent(Backup.class);
 			backup.doBackup(new File("backup"));
+			log.info("Backup complete");
+			System.exit(0);
 		}
 		catch (Throwable t) {
 			log.error("Backup failure", t);
